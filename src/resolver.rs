3a622bc854c08331eca2402158284b6876587c4b
Nick Ogden <nick@nickogden.org>
2020-07-03 14:21:16 +0100

    Remove unneeded allocations in resolution pass
    The scope maps were holding Strings, requiring identifiers to be
    allocated each time they were added to the map. This was unnecessary
    as the scope maps only live as long as the Resolver, which the source
    code buffer will always outlive.
    
    We now store identifier references removing the allocations.

--- src/resolver.rs
+++ src/resolver.rs
@@ -23,13 +23,13 @@ pub fn resolve_references<'s>(
     Ok(resolver.resolved)
 }
 
-struct Resolver {
-    scopes: Vec<HashMap<String, bool>>,
+struct Resolver<'s> {
+    scopes: Vec<HashMap<&'s str, bool>>,
     resolved: ReferenceTable,
 }
 
-impl Resolver {
-    fn resolve_statement<'s>(
+impl<'s> Resolver<'s> {
+    fn resolve_statement(
         &mut self, statement: StmtIndex, ast: &Ast<'s>
     ) -> Result<(), ParseError<'s>> {
         use crate::parser::Statement::*;
@@ -84,7 +84,7 @@ impl Resolver {
         Ok(())
     }
 
-    fn resolve_expression<'s>(
+    fn resolve_expression(
         &mut self, expression: ExprIndex, ast: &Ast<'s>
     ) -> Result<(), ParseError<'s>> {
         use crate::parser::Expression::*;
@@ -134,7 +134,7 @@ impl Resolver {
         Ok(())
     }
 
-    fn resolve_fn<'s>(
+    fn resolve_fn(
         &mut self, parameters: &Vec<Token<'s>>, body: &Vec<StmtIndex>, ast: &Ast<'s>
     ) -> Result<(), ParseError<'s>> {
         self.scopes.push(HashMap::new());
@@ -149,19 +149,19 @@ impl Resolver {
         Ok(())
     }
 
-    fn declare(&mut self, identifier: &str) {
+    fn declare(&mut self, identifier: &'s str) {
         if let Some(current_scope) = self.scopes.last_mut() {
-            current_scope.insert(identifier.to_owned(), false);
+            current_scope.insert(identifier, false);
         }
     }
 
-    fn define(&mut self, identifier: &str) {
+    fn define(&mut self, identifier: &'s str) {
         if let Some(current_scope) = self.scopes.last_mut() {
-            let _ = current_scope.insert(identifier.to_owned(), true);
+            let _ = current_scope.insert(identifier, true);
         }
     }
 
-    fn resolve(&mut self, expression: ExprIndex, identifier: &str) {
+    fn resolve(&mut self, expression: ExprIndex, identifier: &'s str) {
         for (i, scope) in self.scopes.iter().rev().enumerate() {
             if scope.contains_key(identifier) {
                 let _ = self.resolved.insert(expression, i);
